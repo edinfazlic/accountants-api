50acbf025ca181e9e8b00fe844922a0745ce9e09
Edin Fazlic <edin@medrecord.io>
2021-05-18 17:52:42 +0200

    Changed: allowed another UI to access the backend api (resolving CORS issues) through the env var

--- src/main/java/com/accountant/apiservice/configuration/MyConfiguration.java
+++ src/main/java/com/accountant/apiservice/configuration/MyConfiguration.java
@@ -1,5 +1,6 @@
 package com.accountant.apiservice.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,6 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MyConfiguration {
 
+    private static final String LOCALHOST_4200 = "http://localhost:4200";
+
+    @Value("${service.ui.url}")
+    private String allowedOrigin;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -15,7 +21,7 @@ public class MyConfiguration {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                     .allowedMethods("*")
-                    .allowedOrigins("http://localhost:4200");
+                    .allowedOrigins(LOCALHOST_4200, allowedOrigin);
             }
         };
     }
